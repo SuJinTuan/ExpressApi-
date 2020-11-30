6015c8300fa7f0ab39ab9a57504b80e70072533c
SuJinTuan <61959864+SuJinTuan@users.noreply.github.com>
2020-11-30 15:15:27 +0800

    Update aliconfig.js

--- util/aliconfig.js
+++ util/aliconfig.js
@@ -1,8 +1,8 @@
 // const Core = require('@alicloud/pop-core')
 module.exports = {
   alicloud: {
-    "accessKeyId": 'LTAI4GBZ7dvLgEQkWYRxgCYm',
-    "accessKeySecret": 'mXV0IV6EKnIYaCu7mTYR9Jtdze6ZqR',
+    "accessKeyId": '自己的ID',
+    "accessKeySecret": '自己的密码',
     "endpoint": 'https://dysmsapi.aliyuncs.com',
     "apiVersion": "2017-05-25"
   }
