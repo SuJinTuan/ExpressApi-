9283755613df9a0d1a6958a2916b3a0850a23c58
SuJinTuan <sujin810969050@qq.com>
2020-12-08 10:28:10 +0800

    处理敏感信息

--- util/aliconfig.js
+++ util/aliconfig.js
@@ -1,8 +1,8 @@
 // const Core = require('@alicloud/pop-core')
 module.exports = {
   alicloud: {
-    "accessKeyId": '你自己的ID',
-    "accessKeySecret": '你自己的Secret',
+    "accessKeyId": '你自己的IDs',
+    "accessKeySecret": '你自己的Secrets',
     "endpoint": 'https://dysmsapi.aliyuncs.com',
     "apiVersion": "2017-05-25"
   }
